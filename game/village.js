9b1ecfedb4e29f085c12f1aabf4c4d1790a4bf46
V0II0D <fedor0morozov@gmail.com>
2026-09-06 19:12:17 +0500

    Update village.js

--- game/village.js
+++ game/village.js
@@ -1,5 +1,6 @@
 export function createVillage(){
 const village=document.getElementById("village");
+village.style.borderRadius="24px";
+village.style.borderWidth="10px";
 village.dataset.level="1";
-return village;
 }
