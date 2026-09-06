a1aad628376736bc8fa5f550d0910fe6c073ff52
V0II0D <fedor0morozov@gmail.com>
2026-09-06 20:33:37 +0500

    Update world.js

--- game/world.js
+++ game/world.js
@@ -1,7 +1,7 @@
 export function createWorld(){
 const world=document.getElementById("world");
 const objects=[
-["tree",8,18],["tree",88,12],["tree",14,78],["tree",82,82],["tree",6,48],["tree",94,52],["tree",20,12],["tree",76,8],["tree",4,70],["tree",92,72],["tree",12,32],["tree",86,35],["tree",28,8],["tree",68,10],["tree",8,88],["tree",92,88],["tree",25,92],["tree",74,92],["tree",97,28],["tree",3,25]
+["tree",6,12],["tree",94,10],["tree",5,88],["tree",95,90],["tree",12,22],["tree",88,20],["tree",8,38],["tree",92,40],["tree",5,62],["tree",95,64],["tree",18,8],["tree",82,7],["tree",18,94],["tree",82,94],["tree",28,6],["tree",72,6],["tree",28,96],["tree",72,96],["tree",2,30],["tree",98,30],["tree",2,75],["tree",98,78]
 ];
 objects.forEach(([type,x,y])=>{
 const el=document.createElement("div");
