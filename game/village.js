20fb4f38c5a46f5be5d00e2e805fe4592eadc056
V0II0D <fedor0morozov@gmail.com>
2026-09-06 20:24:34 +0500

    Update village.js

--- game/village.js
+++ game/village.js
@@ -10,7 +10,6 @@ createForge(village,80,25);
 createHouse(village,25,72);
 createHouse(village,75,72);
 createWell(village,50,82);
-createFence(village)
 }
 function createMainBuilding(p){
 const e=document.createElement("img");
@@ -55,20 +54,5 @@ p.appendChild(e)
 function createPath(p,c){
 const e=document.createElement("div");
 e.className=c;
-p.appendChild(e)
-}
-function createFence(p){
-const f=document.createElement("div");
-f.id="fence";
-for(let x=8;x<=92;x+=8){piece(f,x,0);piece(f,x,100)}
-for(let y=8;y<=92;y+=8){piece(f,0,y);piece(f,100,y)}
-p.appendChild(f)
-}
-function piece(p,x,y){
-const e=document.createElement("img");
-e.src="game/objects/fence.svg";
-e.className="fencePiece";
-e.style.left=x+"%";
-e.style.top=y+"%";
 p.appendChild(e)
 }
