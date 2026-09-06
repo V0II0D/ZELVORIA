81cbfaaafceceb3ba22fab31d7dab439afcf3589
V0II0D <fedor0morozov@gmail.com>
2026-09-06 20:20:24 +0500

    Update world.js

--- game/world.js
+++ game/world.js
@@ -1,9 +1,7 @@
 export function createWorld(){
 const world=document.getElementById("world");
 const objects=[
-["tree",8,18],["tree",88,12],["tree",14,78],["tree",82,82],["tree",6,48],["tree",94,52],["tree",20,12],["tree",76,8],["tree",4,70],["tree",92,72],["tree",12,32],["tree",86,35],["tree",28,8],["tree",68,10],["tree",8,88],["tree",92,88],["tree",25,92],["tree",74,92],["tree",97,28],["tree",3,25],
-["rock",24,14],["rock",72,18],["rock",18,62],["rock",78,68],
-["water",68,45],["water",30,88]
+["tree",8,18],["tree",88,12],["tree",14,78],["tree",82,82],["tree",6,48],["tree",94,52],["tree",20,12],["tree",76,8],["tree",4,70],["tree",92,72],["tree",12,32],["tree",86,35],["tree",28,8],["tree",68,10],["tree",8,88],["tree",92,88],["tree",25,92],["tree",74,92],["tree",97,28],["tree",3,25]
 ];
 objects.forEach(([type,x,y])=>{
 const el=document.createElement("div");
