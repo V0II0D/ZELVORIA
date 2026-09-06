eeda52a5a94649893f00499d2ff9a9ff7bbb0683
V0II0D <fedor0morozov@gmail.com>
2026-09-06 19:28:12 +0500

    Update main.js

--- game/main.js
+++ game/main.js
@@ -1,18 +1,15 @@
-import {createVillage} from “./village.js”;
-import {createWorld} from “./world.js”;
-let silver=0;
-let gold=0;
-let wave=1;
-let villageLevel=1;
-const silverEl=document.getElementById(“silver”);
-const goldEl=document.getElementById(“gold”);
-const waveEl=document.getElementById(“wave”);
-const villageLevelEl=document.getElementById(“villageLevel”);
+import{createVillage}from’./village.js’;
+import{createWorld}from’./world.js’;
+let silver=0,gold=0,wave=1,villageLevel=1;
+const silverEl=document.getElementById(‘silver’);
+const goldEl=document.getElementById(‘gold’);
+const waveEl=document.getElementById(‘wave’);
+const villageLevelEl=document.getElementById(‘villageLevel’);
 createWorld();
 createVillage();
-document.getElementById(“gameShop”).onclick=()=>alert(“Игровой магазин”);
-document.getElementById(“customShop”).onclick=()=>alert(“Магазин кастомизации”);
-document.getElementById(“pause”).onclick=()=>alert(“Пауза”);
+document.getElementById(‘gameShop’).onclick=()=>alert(‘Игровой магазин’);
+document.getElementById(‘customShop’).onclick=()=>alert(‘Магазин кастомизации’);
+document.getElementById(‘pause’).onclick=()=>alert(‘Пауза’);
 function updateUI(){
 silverEl.textContent=silver;
 goldEl.textContent=gold;
