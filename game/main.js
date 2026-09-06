a4823309a2f5498550d1263e92d8e65109a2c740
V0II0D <fedor0morozov@gmail.com>
2026-09-06 19:06:10 +0500

    Create main.js

---
+++ game/main.js
@@ -0,0 +1,18 @@
+let silver=0;
+let gold=0;
+let wave=1;
+let villageLevel=1;
+const silverEl=document.getElementById("silver");
+const goldEl=document.getElementById("gold");
+const waveEl=document.getElementById("wave");
+const villageLevelEl=document.getElementById("villageLevel");
+document.getElementById("gameShop").onclick=()=>alert("Игровой магазин");
+document.getElementById("customShop").onclick=()=>alert("Магазин кастомизации");
+document.getElementById("pause").onclick=()=>alert("Пауза");
+function updateUI(){
+silverEl.textContent=silver;
+goldEl.textContent=gold;
+waveEl.textContent=wave;
+villageLevelEl.textContent=villageLevel;
+}
+updateUI();
