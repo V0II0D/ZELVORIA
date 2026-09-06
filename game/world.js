6c4811f568d3cc9f7b449f011b257740c871d4fb
V0II0D <fedor0morozov@gmail.com>
2026-09-06 19:49:33 +0500

    Update world.js

--- game/world.js
+++ game/world.js
@@ -1,9 +1,21 @@
 export function createWorld(){
 const world=document.getElementById("world");
 const objects=[
-["tree","🌲",8,18],["tree","🌲",88,12],["tree","🌲",14,78],["tree","🌲",82,82],["tree","🌲",6,48],["tree","🌲",94,52],
-["rock","🪨",24,14],["rock","🪨",72,18],["rock","🪨",18,62],["rock","🪨",78,68],
-["water","💧",68,45],["water","💧",30,88]
+["tree",8,18],["tree",88,12],["tree",14,78],["tree",82,82],["tree",6,48],["tree",94,52],["tree",20,12],["tree",76,8],["tree",4,70],["tree",92,72],["tree",12,32],["tree",86,35],["tree",28,8],["tree",68,10],["tree",8,88],["tree",92,88],["tree",25,92],["tree",74,92],["tree",97,28],["tree",3,25],
+["rock",24,14],["rock",72,18],["rock",18,62],["rock",78,68],
+["water",68,45],["water",30,88]
 ];
-objects.forEach(([type,icon,x,y])=>{const el=document.createElement("div");el.className=`worldObject ${type}`;el.textContent=icon;el.style.left=x+"%";el.style.top=y+"%";world.appendChild(el);});
+objects.forEach(([type,x,y])=>{
+const el=document.createElement("div");
+el.className=`worldObject ${type}`;
+el.style.left=x+"%";
+el.style.top=y+"%";
+if(type==="tree"){
+const img=document.createElement("img");
+img.src="game/objects/tree.svg";
+el.appendChild(img);
+}else if(type==="rock")el.textContent="🪨";
+else if(type==="water")el.textContent="💧";
+world.appendChild(el);
+});
 }
